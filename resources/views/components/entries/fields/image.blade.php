a61972824ad644778e45f7789aa69ae31ff95dbe
Hector Moya <nokure@gmail.com>
2025-11-11 21:40:18 +1000

    wip

--- resources/views/components/entries/fields/image.blade.php
+++ resources/views/components/entries/fields/image.blade.php
@@ -1,1 +1,53 @@
-../../field-types/image.blade.php
+@php
+    $value = $form->fieldValues[$field->handle] ?? null;
+    $asset = $value ? \App\Models\Asset::find($value) : null;
+@endphp
+
+<div>
+    <div class="space-y-2">
+        {{-- Label & Instructions --}}
+        <div>
+            <flux:heading size="sm">{{ $field->label }}</flux:heading>
+            @if($field->instructions)
+                <flux:description>{{ $field->instructions }}</flux:description>
+            @endif
+        </div>
+
+        @if($asset)
+            <div class="relative group aspect-video w-48 rounded-lg border-2 border-gray-200 overflow-hidden">
+                <img
+                    src="{{ $asset->url }}"
+                    alt="{{ $asset->alt_text }}"
+                    class="w-full h-full object-cover"
+                />
+                <button
+                    type="button"
+                    wire:click="form.fieldValues.{{ $field->handle }} = null"
+                    class="absolute top-2 right-2 p-1 bg-white rounded-full shadow opacity-0 group-hover:opacity-100 transition-opacity"
+                >
+                    <flux:icon.x-mark class="w-4 h-4 text-gray-500" />
+                </button>
+            </div>
+        @endif
+
+        {{-- Asset Selector Button --}}
+        <div>
+            <flux:button
+                type="button"
+                variant="ghost"
+                wire:click="openAssetBrowser('{{ $field->handle }}')"
+            >
+                <flux:icon name="photo" class="w-4 h-4 mr-2" />
+                {{ $asset ? 'Change Image' : 'Select Image' }}
+            </flux:button>
+        </div>
+
+        {{-- Error State --}}
+        <flux:error name="form.fieldValues.{{ $field->handle }}" />
+
+        {{-- Asset Browser Modal --}}
+        <flux:modal name="asset-browser-{{ $field->handle }}">
+            <livewire:assets.browser :key="'asset-browser-' . $field->handle" :field-handle="$field->handle" />
+        </flux:modal>
+    </div>
+</div>
--- resources/views/livewire/entries/edit.blade.php
+++ resources/views/livewire/entries/edit.blade.php
@@ -39,7 +39,7 @@
 
                     @foreach ($this->blueprint->fields as $field)
                             <div>
-                                <x-dynamic-component component="entries.fields.image" :field="$field" />
+                                <x-dynamic-component :component="'entries.fields.' . $field->type" :field="$field" />
                                 {{-- @includeIf('entries.fields.' . $field->type, ['field' => $field]) --}}
                             </div>
                     @endforeach
